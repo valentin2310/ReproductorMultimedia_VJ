c28d2af631a4a3c6ccfbcfb112aac85b52be52c7
ZheiosAC <elvaliac23@gmail.com>
2023-01-30 13:52:11 +0100

    activity menu y bottomnavigationmenu

--- app/src/main/java/com/example/reproductormultimedia_vj/MainActivity.java
+++ app/src/main/java/com/example/reproductormultimedia_vj/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.reproductormultimedia_vj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,7 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-// mira mis wevos
-        //sexo
+
+
+    }
+
+    public void iniciarSesion(View view){
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
